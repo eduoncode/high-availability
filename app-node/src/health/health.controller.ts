4adf71dc84cf858f05ca33c05ba9d2d4abdde204
Eduardo Fernandes <edufernandes.1895gmail.com>
2025-10-29 15:25:41 -0300

    feat: adicionar suporte a health checks e otimizações no Nginx, Dockerfile e controlador de saúde

--- app-node/src/health/health.controller.ts
+++ app-node/src/health/health.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Logger } from '@nestjs/common';
 
 @Controller('health')
 export class HealthController {
+  private readonly logger = new Logger(HealthController.name);
   @HttpCode(HttpStatus.OK)
   @Get()
   async healthCheck(): Promise<{ status: string }> {
+    this.logger.log('Health check requested');
     return { status: 'ok' };
   }
 }
